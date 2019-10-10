244285f5630cc497980bfbee840ee24b188759db
Guayo <guayo29@gmail.com>
2019-10-10 17:43:38 +0200

    removed MCDS v0.5.0

--- v0.5.0/libMCDS/xsde/libxsde/xsde/c/compilers/ghs/post.h
+++
@@ -1,9 +0,0 @@
-/* file      : xsde/c/compilers/ghs/post.h
- * author    : Boris Kolpackov <boris@codesynthesis.com>
- * copyright : Copyright (c) 2005-2011 Code Synthesis Tools CC
- * license   : GNU GPL v2 + exceptions; see accompanying LICENSE file
- */
-
-#ifdef __EDG__
-#  pragma ghs endnowarning
-#endif
--- v0.5.0/libMCDS/xsde/libxsde/xsde/cxx/compilers/vc-6/post.hxx
+++
@@ -1,6 +0,0 @@
-// file      : xsde/cxx/compilers/vc-6/post.hxx
-// author    : Boris Kolpackov <boris@codesynthesis.com>
-// copyright : Copyright (c) 2005-2011 Code Synthesis Tools CC
-// license   : GNU GPL v2 + exceptions; see accompanying LICENSE file
-
-#pragma warning (pop)
