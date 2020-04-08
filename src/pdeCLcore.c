1e741d208fdba3bc4efcf7f652127a36a358e1bf
Guayo Peerdeman <guayo@live.nl>
2020-04-08 21:16:34 +0200

    Revert "Fixed boundary bug"
    This reverts commit 3e2758ec9d424944550cf9e0cced5b5b04b4ddc1.

--- src/pdeCLcore.c
+++ src/pdeCLcore.c
@@ -18,26 +18,28 @@ int btype )
   //Calculate position in aray
   int layersize = xsize * ysize;
   int zpos = id/layersize;
-  int xpos = (id - (zpos * layersize)) /ysize;
-  int ypos = id - xpos * ysize - zpos * layersize; 
-  btype = 2;
+  int ypos = (id - (zpos * layersize)) /xsize;
+  int xpos = id - ypos * xsize - zpos * layersize; 
+
+  
+  btype = 1;
   //Boundaries
   double sum =0.;
-  if (xpos == 0 || ypos == 0 || xpos == xsize-1 || ypos == ysize-1){
+  if (xpos == 0 || ypos == 0 || xpos == xsize || ypos == ysize){
     switch(btype){
     case 1:
-    //Noflux gradient
-    if (ypos == ysize-1){ sigmaB[id] = 1.;} 
+    //Noflux
+    if (ypos == ysize) sigmaB[id] = 1.;
     if (ypos == 0) sigmaB[id] = 0.;
-    if (xpos == xsize-1) sigmaB[id] = sigmaA[id-ysize];
-    if (xpos == 0) sigmaB[id] = sigmaA[id+ysize];
+    if (xpos == xsize) sigmaB[id] = sigmaA[id-1];
+    if (xsize == 0) sigmaB[id] = sigmaB[id+1];
     break;
     //Periodic
     case 2:
-    if (xpos == xsize-1) sigmaB[id] = sigmaA[id-layersize+ysize];
-    if (xpos == 0) sigmaB[id] = sigmaA[id+layersize-ysize];
-    if (ypos == ysize-1) sigmaB[id] = sigmaA[id-ysize+1];
-    if (ysize == 0) sigmaB[id] = sigmaB[id+ysize-1]; 
+    if (ypos == ysize) sigmaB[id] = sigmaA[id-layersize+xsize];
+    if (ypos == 0) sigmaB[id] = sigmaA[id+layersize-xsize];
+    if (xpos == xsize) sigmaB[id] = sigmaA[id-xsize+1];
+    if (xsize == 0) sigmaB[id] = sigmaB[id+xsize-1]; 
     break;
     //Absorbing
     case 3:
@@ -51,16 +53,15 @@ int btype )
   double value = sigmaA[id];
   
  //Secretion
-  if (btype != 1){
-    if (zpos == 0){
-      if (sigmacells[id] > 0){
-        value = value + secr_rate * dt;
-      }
-      else{
-         value = value - decay_rate*dt*value;
-      }
+  if (zpos == 0){
+    if (sigmacells[id] > 0){
+      value = value + secr_rate * dt;
+    }
+    else{
+       value = value - decay_rate*dt*value;
     }
   }
+
     //Diffusion
     sum += sigmaA[id-1];
     sum += sigmaA[id+1];
@@ -69,7 +70,5 @@ int btype )
     sum-=4*value;
     sigmaB[id]= value+sum*dt*diff_coeff[zpos]/dx2;
   }
-//if (xpos == xsize -1 && ypos == ysize-1){sigmaB[id] = 1.;}
-
 }
 
