4ee73d85bf23a40ffe99c3cfc737adac8e50af87
Koen Keijzer <koen19@gmail.com>
2024-03-12 16:27:30 +0100

    Added files to create initial state

---
+++ src/cellular_potts/domaininit.cpp
@@ -0,0 +1,130 @@
+#include "domaininit.hpp"
+#include "neighbours.hpp"
+#include "parameter.hpp"
+#include "random.hpp"
+#include "grid.hpp"
+#include <unordered_map>
+#include <vector>
+#include <exception>
+
+extern Parameter par;
+
+
+// Function doesn't work when there are already cells in the dish.
+int GrowInCellsInRectangle(Grid &grid, int init_cells, int cell_size,
+                                          PixelPos upper_left,
+                                          PixelPos lower_right)
+{
+    std::unordered_map<int, std::vector<PixelPos>> initial_positions;
+
+    // Put in inital cells, then do Eden Growth
+    // Index is also the cell_number.
+    int offset_x = upper_left.x;
+    int offset_y = upper_left.y;
+    for (int i = 1; i <= init_cells; i++)
+    {
+        auto deltaX = lower_right.x - upper_left.x;
+        auto deltaY = lower_right.y - upper_left.y;
+        std::cerr << "deltaX " << deltaX << "deltaY " << deltaY << "offsetX "
+                  << offset_x << "offsetY " << offset_y << "\n";
+        int x = RandomNumber(deltaX) + offset_x - 1;
+        int y = RandomNumber(deltaY) + offset_y - 1;
+
+        std::cerr << "Initial positions for cell " << i << " = "
+                  << PixelPos(x, y) << "\n";
+        initial_positions[i] = {{x, y}};
+    }
+
+    for (int i = 0; i < cell_size; i++)
+    {
+        for (int c = 1; c <= init_cells; c++)
+        {
+            // Get a random position of cell c.
+            auto positions = initial_positions[c];
+            auto pos = positions[RandomNumber(positions.size()) - 1];
+
+            auto neighbour_iterator = Neighbours(pos).begin();
+            for (int k = 1; k < RandomNumber(7); k++)
+                neighbour_iterator++;
+            auto neighbour_pos = *neighbour_iterator;
+            if (neighbour_pos.x < upper_left.x || neighbour_pos.y < upper_left.y ||
+                neighbour_pos.x >= lower_right.x || neighbour_pos.y >= lower_right.y)
+                continue;
+
+            auto neighbour_spin = grid.get(neighbour_pos);
+
+            // Skip if the neighbour not medium
+            if (neighbour_spin != 0)
+                continue;
+
+            // Assert that the cell stay simpliy connected.
+            // Trying to copy pos into neighbour_pos
+            if (not(LocalConnectedness(grid,neighbour_pos, neighbour_spin) &&
+                         LocalConnectedness(grid,neighbour_pos, c)))
+                 {
+                     continue;
+                 }
+            initial_positions[c].push_back(neighbour_pos);
+            grid.set(neighbour_pos, c);
+            std::cerr << "\nConsidering: (" << pos << "->" << neighbour_pos
+                      << ") ";
+        }
+    }
+    
+//    // Now copy the new data to the grid.
+//    for (auto const &spinpos : initial_positions)
+//    {
+//        auto spin = spinpos.first;
+//        auto positions = spinpos.second;
+////        if (positions.size() < cell_size*0.9)
+////          continue;
+//        for (auto const &pos : positions)
+//        {
+//            grid.set(pos, spin);
+//        }
+//    }
+}
+
+
+int PutCellsInRectangle(Grid &grid, int n_cells, int cell_size, PixelPos upper_left, PixelPos lower_right) {
+    int rect_x = std::abs(lower_right.x - upper_left.x);
+    int rect_y = std::abs(lower_right.y - upper_left.y);
+    int rectangle_size = std::abs(upper_left.y - lower_right.y) * std::abs(upper_left.x - lower_right.x);
+
+    int size_of_cell_side = 1+static_cast<int>(std::sqrt(cell_size));
+    int num_cell_x = rect_x / size_of_cell_side ;
+    int num_cell_y = rect_y / size_of_cell_side ;
+    
+    std::cerr << "size_of_cell_side " << size_of_cell_side << std::endl;
+    std::cerr << "num_cell_x " << num_cell_x << std::endl;
+    std::cerr << "num_cell_y " << num_cell_y << std::endl;
+
+    for (int y = upper_left.y; y < lower_right.y; y++) 
+        for (int x = upper_left.x; x < lower_right.x; x++){
+            int spin = 1 + (x - upper_left.x) / size_of_cell_side
+                       + ((y-upper_left.y) / size_of_cell_side) * num_cell_x;
+            std::cerr << "(" << x << ',' << y << ':' << spin << ')';
+            if (spin <= n_cells)
+                grid.set({x,y}, spin);
+        }
+}
+
+void AddWall(Grid &grid, PixelPos pos)
+{
+    if (pos.x < 0 || pos.y < 0 || pos.x > par.sizex || pos.y > par.sizey)
+    {
+        throw std::out_of_range("AddWall: position is Out of range");
+    }
+
+    for (auto const &nbh : Neighbours(pos))
+    {
+        if (nbh.x < 0 || nbh.y < 0 || nbh.x >= par.sizex || nbh.y >= par.sizey)
+            continue;
+        std::cerr << "Wall at " << nbh.x << "," << nbh.y << "has value"
+                  << grid.get(nbh) << "\n";
+        if (grid.get(nbh) > 0 || grid.get(pos) > 0)
+            throw std::runtime_error(
+                "AddWall: Can't create wall next to (or on) cell.");
+    }
+    grid.set(pos, -1);
+}
---
+++ src/cellular_potts/domaininit.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include "vec2.hpp"
+#include "grid.hpp"
+
+/**
+ * @brief Grow in specified number of cells in the specified rectangle.
+ * @param n_cells Number of cells that is grown in.
+ * @param cell_size The maximum size the cells have in the beginning.
+ * @param upper_left Upper left coordinate of the rectangle.
+ * @param lower_right Lower right coordinate of the rectangle.
+ * @return Index of the last cell that was inserted.
+*/
+int GrowInCellsInRectangle(Grid &grid, int n_cells, int cell_size, PixelPos upper_left, PixelPos lower_right);
+
+/**
+ * @brief Put rectangular cells in rectangle
+ * @param n_cells Number of cells that is grown in.
+ * @param cell_size The maximum size the cells have in the beginning.
+ * @param upper_left Upper left coordinate of the rectangle.
+ * @param lower_right Lower right coordinate of the rectangle.
+ * @return Index of the last cell that was inserted.
+*/
+int PutCellsInRectangle(Grid &grid, int n_cells, int cell_size, PixelPos upper_left, PixelPos lower_right);
+  
+  
+/**
+ * @brief Create a wall somewere in the grid.
+ * 
+ * The function creates a wall at specified positions. 
+ * Be sure that there are no cells in a neighbourhood of PixelPos,
+ * as this function does not update the edgelist. Meaning that
+ * if a wall is added in the neighbourhood of a cell, the MC algorithm
+ * will break down.
+ *
+ * @param pos 
+ * @throw std::runtime_error when there is a cell in the neighbourhood pos.
+ * @throw std::out_of_range when pos is not within the grid.
+*/
+void AddWall(Grid &grid, PixelPos pos);
