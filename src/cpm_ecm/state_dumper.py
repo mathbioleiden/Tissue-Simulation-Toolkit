fa8549822e02d605ba0a785e61c71105cbbd52ca
Koen Keijzer <koen19@gmail.com>
2025-08-19 09:30:40 +0200

    Increase number of saves

--- src/cpm_ecm/state_dumper.py
+++ src/cpm_ecm/state_dumper.py
@@ -1,5 +1,5 @@
-"""Model state dumper
-"""
+"""Model state dumper"""
+
 import logging
 import pickle
 
@@ -11,32 +11,31 @@ from ymmsl import Operator
 _logger = logging.getLogger(__name__)
 
 
-
 def main() -> None:
     logging.basicConfig(level=logging.DEBUG)
 
-    instance = Instance({
-        Operator.F_INIT: ['cpm_state_in', 'ecm_state_in']})
+    instance = Instance({Operator.F_INIT: ["cpm_state_in", "ecm_state_in"]})
 
     while instance.reuse_instance():
-        Lx = instance.get_setting('Lx', 'float')
-        Ly = instance.get_setting('Ly', 'float')
+        Lx = instance.get_setting("Lx", "float")
+        Ly = instance.get_setting("Ly", "float")
 
-        cpm_state_msg = instance.receive('cpm_state_in')
-        ecm_state_msg = instance.receive('ecm_state_in')
+        cpm_state_msg = instance.receive("cpm_state_in")
+        ecm_state_msg = instance.receive("ecm_state_in")
 
         mcs = int(cpm_state_msg.timestamp)
 
         snapshot = {
-                'Lx': Lx,
-                'Ly': Ly,
-                'mcs': mcs,
-                'cpm_state': cpm_state_msg.data,
-                'ecm_state': ecm_state_msg.data}
+            "Lx": Lx,
+            "Ly": Ly,
+            "mcs": mcs,
+            "cpm_state": cpm_state_msg.data,
+            "ecm_state": ecm_state_msg.data,
+        }
 
-        with open(f'state_{mcs:05d}.pickle', 'wb') as f:
+        with open(f"state_{mcs:07d}.pickle", "wb") as f:
             pickle.dump(snapshot, f)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
