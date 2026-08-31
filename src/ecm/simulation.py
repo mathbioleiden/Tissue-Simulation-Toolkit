8ff66a5e7a87fb57df0e13d4977d73894ff94486
mathbioleiden <roelandmerks@gmail.com>
2026-08-31 17:13:18 +0200

    Corrected typos in the update

--- src/ecm/simulation.py
+++ src/ecm/simulation.py
@@ -105,24 +105,24 @@ class Boundary:
         new_bond_ids = set()
         new_angle_cst_ids = set()
 
-        matches = np.inid(snapshot.bonds.group[:, 0], par_ids)
+        matches = np.isin(snapshot.bonds.group[:, 0], par_ids)
         new_bond_ids.update(snapshot.bonds.tag[matches])
         new_par_ids.update(snapshot.bonds.group[matches, 1])
 
-        matches = np.inid(snapshot.bonds.group[:, 1], par_ids)
+        matches = np.isin(snapshot.bonds.group[:, 1], par_ids)
         new_bond_ids.update(snapshot.bonds.tag[matches])
         new_par_ids.update(snapshot.bonds.group[matches, 0])
 
-        matches = np.inid(snapshot.angles.group[:, 0], par_ids)
+        matches = np.isin(snapshot.angles.group[:, 0], par_ids)
         new_angle_cst_ids.update(snapshot.angles.tag[matches])
         new_par_ids.update(int_array_to_set(snapshot.angles.group[matches, 1:3]))
 
-        matches = np.inid(snapshot.angles.group[:, 1], par_ids)
+        matches = np.isin(snapshot.angles.group[:, 1], par_ids)
         new_angle_cst_ids.update(snapshot.angles.tag[matches])
         new_par_ids.update(snapshot.angles.group[matches, 0])
         new_par_ids.update(snapshot.angles.group[matches, 2])
 
-        matches = np.inid(snapshot.angles.group[:, 2], par_ids)
+        matches = np.isin(snapshot.angles.group[:, 2], par_ids)
         new_angle_cst_ids.update(snapshot.angles.tag[matches])
         new_par_ids.update(int_array_to_set(snapshot.angles.group[matches, 0:2]))
 
